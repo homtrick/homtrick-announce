943f4cf937b645bebf747ef9d6e92f8e37d29877
homtrick <140127272+homtrick@users.noreply.github.com>
2024-08-17 01:52:59 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Homtrick'
+description 'Announcement Script'
+lua54 'yes'
+    
+server_scripts {
+	'server/*.lua',
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/*.lua',
+}
